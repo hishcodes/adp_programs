be7b1acf3bf74151f800c64ee9fa92e56381f438
Hisham Moideen <69593172+hishcodes@users.noreply.github.com>
2022-11-20 11:08:37 +0530

    added comments to quiz.py

--- lab/quiz.py
+++ lab/quiz.py
@@ -15,14 +15,14 @@ capitals = {'Karnataka':'Bangalore',
 }
 
 for key,value in capitals.items():
-    t.write("\n"+str(q_number) + ". What is the capital of "+str(key)+"?\n")
-    q_number += 1
-    correct_answer = value
-    all_options = list(capitals.values())
-    all_options.remove(correct_answer)
-    wrong_options = all_options
-    printable_options = random.sample(wrong_options, 3)
-    printable_options = printable_options + [correct_answer]
-    random.shuffle(printable_options)
+    t.write("\n"+str(q_number) + ". What is the capital of "+str(key)+"?\n")    #prints questions
+    q_number += 1   #increments question numbers in the output
+    correct_answer = value #set current question's answer as correct_answer
+    all_options = list(capitals.values()) #store all the answers in all_options
+    all_options.remove(correct_answer)  #remove the correct_answer from all_options
+    wrong_options = all_options #store the list of wrong options in wrong_options
+    printable_options = random.sample(wrong_options, 3) #randomly pick 3 wrong options
+    printable_options = printable_options + [correct_answer]    #add correct answer to the list of wrong answers
+    random.shuffle(printable_options)   #shuffle the 4 options
 
     t.write("a. "+ str(printable_options[0]) +" b. "+str(printable_options[1])+" c. "+ str(printable_options[2])+" d. "+ str(printable_options[3]))
